ea4fa258cefdb74962d6bb5b25b287d15f6d4db0
pepe1973 <bodrogipr@gmail.com>
2021-03-11 10:43:55 +0100

    FileBeolvas osztály létrehozása

---
+++ Hegyek/FileKezeles/FileBeolvas.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Hegyek.FileKezeles
+{
+    class FileBeolvas
+    {
+        public string FileNev { get; set; }
+
+        public FileBeolvas(string fileNev)
+        {
+            this.FileNev = fileNev;
+        }
+
+        public List<string[]> Beolvas(char delimiter)
+        {
+            List<string[]> lista = new List<string[]>();
+
+            using (StreamReader sr = new StreamReader(this.FileNev, Encoding.UTF8))
+            {
+                string elsoSor = sr.ReadLine();
+
+                while (!sr.EndOfStream)
+                {
+                    string sor = sr.ReadLine().Trim();
+                    string[] tomb = sor.Split(delimiter);
+                    lista.Add(tomb);
+                }
+            }
+
+            return lista;
+        }
+    }
+}
--- Hegyek/Program.cs
+++ Hegyek/Program.cs
@@ -10,7 +10,7 @@ namespace Hegyek
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
             Console.ReadKey(true);
         }
     }
