0be541a0af197f5a4256fd166f900f05ede7be39
pepe1973 <bodrogipr@gmail.com>
2021-03-11 10:16:30 +0100

    Projekt létrehozása

---
+++ Hegyek/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hegyek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
